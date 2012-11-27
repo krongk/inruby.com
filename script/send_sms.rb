b162347dfca2413f875b68f3ca91ef2a8f96dbe6
krongk <kenrome@gmail.com>
2012-11-27 19:55:42 +0800

    seo

--- script/send_sms.rb
+++ script/send_sms.rb
@@ -1,3 +1,6 @@
+#encoding: utf-8
+#remember to install: gem install sms_bao
+
 require 'rubygems'
 require 'active_record'
 require 'sms_bao'
@@ -21,28 +24,37 @@ LocalBase.establish_connection({
 LocalBase.connection.execute("set names 'utf8'")
 
 class Company < LocalBase
-  self.tabe_name = 'company_01'
+  self.table_name = 'company_06'
 end
 
 
 class TestSms
 
   def run
-    puts Company.count
-    Company.where(:is_sms_processed => 'n').find_each do |item|
-      #13980669356
+    puts "-----------------------"
+    puts "total sms acount:"
+    puts SmsBao.query('inruby', 'kenrome001')
+    puts "total reocrds: " + Company.count.to_s
+    puts "starting........"
+    Company.where(:is_sms_processed => 'n').each_with_index do |item, index|
       flag = 'y'
+      content = "成都红宝石信息网(www.inruby.com)提供专业企业信息化,网站建设,网络推广,企业品牌设计,演示文稿,软文写作推广,微博营销,淘宝开店."
       phone = item.mobile_phone.to_s.strip
+      #phone = '15928661802'
       if phone =~ /^1\d{10}$/
          flag = SmsBao.send('inruby', 'kenrome001', phone, content)
       else
         flag = 'bad_phone'
       end
       item.is_sms_processed = flag
-      item.sms_processed_at = Time.now
+      item.processed_at = Time.now
       item.sms_content = content
       item.save!
+      puts "#{phone.ljust(11, ' ')}: #{flag}"
+      break if index > 1000
     end
+    puts "remain sms acount:"
+    puts SmsBao.query('inruby', 'kenrome001')
   end
 end
 
