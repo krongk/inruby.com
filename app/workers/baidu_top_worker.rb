508e959237a12a9a61b486980b234c01922e9f6d
krongk <kenrome@gmail.com>
2012-11-30 23:46:13 +0800

    baid top

--- app/workers/baidu_top_worker.rb
+++ app/workers/baidu_top_worker.rb
@@ -4,7 +4,7 @@ class BaiduTopWorker
   def perform
   	bt = BackgroundJob.find_or_create_by_name('baidu_top')
   	#fetch baidu top onice a day
-  	if (Time.now - bt.updated_at > 400)
+  	if (Time.now - bt.updated_at > 86400)
   	  system "rake baidu_top:forager"
   	  bt.frequance_count += 1
   	  bt.save!
